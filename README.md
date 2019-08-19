a052b087d857dc42109b6babbf134ae0371e6472
Saúl Piña <saul.pina@ingeint.com>
2019-08-19 10:39:49 -0500

    Update deployer

--- README.md
+++ README.md
@@ -1,6 +1,25 @@
 # iDempiere Plugin Deployer
 
+This tool allows you connect to iDempiere's OSGI platform and deploy a plugin, it's useful for continuous integration platforms.
+
+### Usage:
+
 ```
-./osgiPluginDeployer.sh host port bundleName level serverJarPath
-./osgiPluginDeployer.sh 127.0.0.1 12612 com.ingeint.template 5 /plugins/com.ingeint.template-6.2.0-SNAPSHOT.jar
+Usage:
+    ./deployer -h
+                [Display this help message]
+    ./deployer.sh ss -h <host> -p <port>
+                [Show plugins list]
+    ./deployer.sh id -h <host> -p <port> -n <name>
+                [Show plugin's id]
+    ./deployer.sh status -h <host> -p <port> -n <name>
+                [Show plugin's status]
+    ./deployer.sh deploy -h <host> -p <port> -n <name> -l <level> -j <jar>
+                [Deploy a plugin]
+```
+
+### Example:
+
+```
+./deployer.sh deploy -h 127.0.0.1 -p 12612 -n com.ingeint.template -l 5 -j /plugins/com.ingeint.template-6.2.0-SNAPSHOT.jar
 ```
---
+++ deployer.sh
@@ -0,0 +1,264 @@
+#!/usr/bin/env bash
+
+set -Eeo pipefail
+
+function validateHost() {
+    if [[ "${host}" == "" ]] ; then
+        echo "Invalid Option: $subcommand requires an argument -h"
+        exit 1
+    fi
+}
+
+function validatePort() {
+    if [[ "${port}" == "" ]] ; then
+        echo "Invalid Option: $subcommand requires an argument -p"
+        exit 1
+    fi
+}
+
+function validateBundleName() {
+    if [[ "${bundleName}" == "" ]] ; then
+        echo "Invalid Option: $subcommand requires an argument -n"
+        exit 1
+    fi
+}
+
+function validateLevel() {
+    if [[ "${level}" == "" ]] ; then
+        echo "Invalid Option: $subcommand requires an argument -l"
+        exit 1
+    fi
+}
+
+function validateJarFile() {
+    if [[ "${jarFile}" == "" ]] ; then
+        echo "Invalid Option: $subcommand requires an argument -j"
+        exit 1
+    fi
+}
+
+function ss() {
+    expect << EOF
+spawn telnet ${host} ${port}
+expect -re "osgi>"
+send "ss\r"
+expect -re "osgi>"
+send "disconnect\r"
+EOF
+}
+
+function getId() {
+    ss | grep "${bundleName}_" | awk '{print $1}'
+}
+
+function getStatus() {
+    ss | grep "${bundleName}_" | awk '{print $2}'
+}
+
+function install() {
+    expect << EOF
+spawn telnet ${host} ${port}
+expect -re "osgi>"
+send "install file:${jarFile}\r"
+expect -re "osgi>"
+send "disconnect\r"
+EOF
+}
+
+function uninstall() {
+    expect << EOF
+spawn telnet ${host} ${port}
+expect -re "osgi>"
+send "uninstall $(getId)\r"
+expect -re "osgi>"
+send "disconnect\r"
+EOF
+}
+
+function setbsl() {
+    expect << EOF
+spawn telnet ${host} ${port}
+expect -re "osgi>"
+send "setbsl ${level} $(getId)\r"
+expect -re "osgi>"
+send "disconnect\r"
+EOF
+}
+
+function start() {
+    expect << EOF
+spawn telnet ${host} ${port}
+expect -re "osgi>"
+send "start $(getId)\r"
+expect -re "osgi>"
+send "disconnect\r"
+EOF
+}
+
+function deploy() {
+    uninstall
+    sleep 2
+    install
+    sleep 2
+    setbsl
+    sleep 2
+    start
+    sleep 2
+
+    echo "$(ss | grep "${bundleName}_")"
+
+    if [[ "$(getStatus)" != "ACTIVE" ]] ; then
+        echo "Status is not active" ; exit 1
+    fi
+}
+
+function help() {
+    echo "Usage:"
+    printf "    Display this help message:\n                ./deployer -h\n"
+    printf "    Show plugins list:\n                ./deployer.sh ss -h <host> -p <port>\n"
+    printf "    Show plugin's id:\n                ./deployer.sh id -h <host> -p <port> -n <name>\n"
+    printf "    Show plugin's status:\n                ./deployer.sh status -h <host> -p <port> -n <name>\n"
+    printf "    Deploy a plugin:\n                ./deployer.sh deploy -h <host> -p <port> -n <name> -l <level> -j <jar>\n"
+    exit 0
+}
+
+if [[ $# == 0 ]] ; then
+    help
+fi
+
+while getopts ":h" opt; do
+    case ${opt} in
+        h )
+            help
+            ;;
+        ? )
+            echo "Invalid Option: -$OPTARG" 1>&2
+            exit 1
+            ;;
+    esac
+done
+shift $((OPTIND -1))
+
+subcommand=$1; shift
+case "$subcommand" in
+    ss)
+        while getopts ":h:p:" opt; do
+            case ${opt} in
+                h )
+                    host=$OPTARG
+                    ;;
+                p )
+                    port=$OPTARG
+                    ;;
+                : )
+                    echo "Invalid Option: -$OPTARG requires an argument" 1>&2
+                    exit 1
+                    ;;
+                ? )
+                    echo "Invalid Option: -$OPTARG" 1>&2
+                    exit 1
+                    ;;
+            esac
+        done
+        shift $((OPTIND -1))
+        validateHost
+        validatePort
+        ss
+        ;;
+    id)
+        while getopts ":h:p:n:" opt; do
+            case ${opt} in
+                h )
+                    host=$OPTARG
+                    ;;
+                p )
+                    port=$OPTARG
+                    ;;
+                n )
+                    bundleName=$OPTARG
+                    ;;
+                : )
+                    echo "Invalid Option: -$OPTARG requires an argument" 1>&2
+                    exit 1
+                    ;;
+                ? )
+                    echo "Invalid Option: -$OPTARG" 1>&2
+                    exit 1
+                    ;;
+            esac
+        done
+        shift $((OPTIND -1))
+        validateHost
+        validatePort
+        validateBundleName
+        getId
+        ;;
+    status)
+        while getopts ":h:p:n:" opt; do
+            case ${opt} in
+                h )
+                    host=$OPTARG
+                    ;;
+                p )
+                    port=$OPTARG
+                    ;;
+                n )
+                    bundleName=$OPTARG
+                    ;;
+                : )
+                    echo "Invalid Option: -$OPTARG requires an argument" 1>&2
+                    exit 1
+                    ;;
+                ? )
+                    echo "Invalid Option: -$OPTARG" 1>&2
+                    exit 1
+                    ;;
+            esac
+        done
+        shift $((OPTIND -1))
+        validateHost
+        validatePort
+        validateBundleName
+        getStatus
+        ;;
+    deploy)
+        while getopts ":h:p:n:l:j:" opt; do
+            case ${opt} in
+                h )
+                    host=$OPTARG
+                    ;;
+                p )
+                    port=$OPTARG
+                    ;;
+                n )
+                    bundleName=$OPTARG
+                    ;;
+                l )
+                    level=$OPTARG
+                    ;;
+                j )
+                    jarFile=$OPTARG
+                    ;;
+                : )
+                    echo "Invalid Option: -$OPTARG requires an argument" 1>&2
+                    exit 1
+                    ;;
+                ? )
+                    echo "Invalid Option: -$OPTARG" 1>&2
+                    exit 1
+                    ;;
+            esac
+        done
+        shift $((OPTIND -1))
+        validateHost
+        validatePort
+        validateBundleName
+        validateLevel
+        validateJarFile
+        deploy
+        ;;
+    *)
+        echo "Invalid Option: $subcommand" 1>&2
+        exit 1
+        ;;
+esac
